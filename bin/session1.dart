1e64789932ff04b591ffa6ff513518635120f1c4
mennasaleh99 <mennasaleh991@gmail.com>
2022-07-17 08:48:01 +0200

    first

---
+++ bin/session1.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:session1/session1.dart' as session1;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${session1.calculate()}!');
+// print('enter word');
+// String w=stdin.readLineSync()!;
+// print("hello $w");
+
+// print('enter number');
+// int w2=int.parse(stdin.readLineSync()!);
+// print("hello $w2");
+// String s='hhd';
+// int x=5;
+// // print(s+'$x');
+// double ss=10.5;
+// num s1=2.5;
+// s1=2;
+// // print(s1.runtimeType);
+
+// bool v=false;
+// bool v2=true;
+
+List<int> l=[1,2,3];
+List<String> l2=['sun','sea'];
+List<dynamic> l3=[1,'sea'];
+ 
+Map m={'Name':'Menna',
+'age':15,
+1:155,
+1:'her index'};
+
+Set sss={1,2,'hala','k',2};
+print(sss.add(7));
+print(sss);
+
+var x=5;
+var x2=5.5;
+var x3="hhhhh";
+var x4=[1,2,4,7];
+var x5={'name':'menna',"id":5};
+var x6=true;
+
+var xx;
+xx=1;
+xx=2.5;
+xx=true;
+xx=['jj',1];
+
+}
---
+++ test/session1_test.dart
@@ -0,0 +1,8 @@
+import 'package:session1/session1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
